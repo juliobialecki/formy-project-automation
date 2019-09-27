f7c01759422c363dcea82664410072a601653c9f
Julio Bialecki <jbialecki@icepor.com>
2019-09-27 14:28:04 +0200

    Initial commit

---
+++ 04_03/start/test/Datepicker.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class Datepicker {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/jbialecki/Downloads/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/datepicker");
+
+        WebElement datepicker = driver.findElement(By.id("datepicker"));
+        datepicker.click();
+
+        WebElement todayDay = driver.findElement(By.className("today"));
+        todayDay.click();
+
+        //driver.quit();
+    }
+}
